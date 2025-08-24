ffab941faba10c4b5373e74721af87b01c09700a
Nathan <nahoang168@gmail.com>
2025-08-24 15:36:50 -0700

    a

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/website-demo/",
+  base: "/website-demo",
   server: {
     watch: {
       usePolling: true,
